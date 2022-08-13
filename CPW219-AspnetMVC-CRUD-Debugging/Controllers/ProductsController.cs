9bdacce40e4a634874dcebe46a2fd86a72f7da41
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-08-13 15:08:34 -0700

    Fixed adding to database

--- CPW219-AspnetMVC-CRUD-Debugging/Controllers/ProductsController.cs
+++ CPW219-AspnetMVC-CRUD-Debugging/Controllers/ProductsController.cs
@@ -28,7 +28,9 @@ namespace CPW219_AspnetMVC_CRUD_Debugging.Controllers
         {
             if (ModelState.IsValid)
             {
-                await _context.AddAsync(product);
+                _context.Product.Add(product);
+                await _context.SaveChangesAsync();
+
                 return RedirectToAction(nameof(Index));
             }
             return View(product);
